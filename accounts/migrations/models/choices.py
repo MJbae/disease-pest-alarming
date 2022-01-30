8d0e7011929580ce8693355d7e7e68802e4db68a
MJbae <bmj5208@gmail.com>
2022-01-30 20:44:36 +0900

    feat: define address choices

--- accounts/migrations/models/choices.py
+++ accounts/migrations/models/choices.py
@@ -4,3 +4,15 @@ from django.db import models
 class GenderChoices(models.TextChoices):
     MALE = "M", "남성"
     FEMALE = "F", "여성"
+
+
+class LargeCategoryAddressChoices(models.TextChoices):
+    SEOUL = "Seoul", "서울특별시"
+    GYEONGGI = "Gyeonggi-do", "경기도",
+
+
+class MediumCategoryAddressChoices(models.TextChoices):
+    HWASEONG = "Hwaseong-si", "화성시"
+    SUWON = "Suwon-si", "수원시"
+    SEONGNAM = "Seongnam-si", "성남시"
+    ANYANG = "Anyang-si", "안양시"
