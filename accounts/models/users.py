4b6cab1ce27af7447948edc24210b4d730ae0d77
MJbae <bmj5208@gmail.com>
2022-02-05 11:40:18 +0900

    feat: add Farm model

--- accounts/models/users.py
+++ accounts/models/users.py
@@ -3,6 +3,7 @@ from django.core.validators import RegexValidator
 from django.db import models
 
 from accounts.models.choices import LargeCategoryAddressChoices, MediumCategoryAddressChoices
+from backend import settings
 
 
 class User(AbstractUser):
@@ -12,8 +13,17 @@ class User(AbstractUser):
         validators=[RegexValidator(r"^010-?[1-9]\d{3}-?\d{4}$")],
     )
 
-    large_category_address = models.CharField(max_length=15, blank=True, choices=LargeCategoryAddressChoices.choices)
-    medium_category_address = models.CharField(max_length=15, blank=True, choices=MediumCategoryAddressChoices.choices)
-
     def get_full_name(self):
         return f"{self.last_name} {self.first_name}".strip()
+
+
+class Farm(models.Model):
+    owner = models.ForeignKey(
+        settings.AUTH_USER_MODEL, related_name="farm_set", on_delete=models.CASCADE
+    )
+    name = models.CharField(
+        max_length=13,
+        blank=True,
+    )
+    large_category_address = models.CharField(max_length=15, blank=True, choices=LargeCategoryAddressChoices.choices)
+    medium_category_address = models.CharField(max_length=15, blank=True, choices=MediumCategoryAddressChoices.choices)
