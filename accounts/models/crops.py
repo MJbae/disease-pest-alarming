d050cf4a6a82157b7b77dd776375387fffee357b
MJbae <bmj5208@gmail.com>
2022-02-05 11:49:15 +0900

    feat: add ProducingCrop model

--- accounts/models/crops.py
+++ accounts/models/crops.py
@@ -1,4 +1,5 @@
 from django.db import models
+from users import Farm
 
 
 class Crop(models.Model):
@@ -8,3 +9,13 @@ class Crop(models.Model):
     code = models.CharField(
         max_length=13,
     )
+
+
+class ProducingCrop(models.Model):
+    farm = models.ForeignKey(
+        Farm, related_name="producing_crop_set", on_delete=models.CASCADE
+    )
+    crop = models.ForeignKey(
+        Crop, related_name="producing_crop_set", on_delete=models.CASCADE
+    )
+    is_in_house = models.BooleanField(default=False)
