9b9cfeb6c3e204ea302921ba4e4e1d1d2a0e2823
MJbae <bmj5208@gmail.com>
2022-02-06 18:49:28 +0900

    feat: add manual update trigger with functional view

--- forecasting/views.py
+++ forecasting/views.py
@@ -1,9 +1,25 @@
 from rest_framework.viewsets import ModelViewSet
 
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.exceptions import ParseError
+
 from forecasting.models import Forecasting
 from forecasting.serializers import ForecastingSerializer
+from forecasting.services.forecasting import refine_xml_from_forecasting_search
 
 
 class ForecastingViewSet(ModelViewSet):
     queryset = Forecasting.objects.all()
     serializer_class = ForecastingSerializer
+
+
+@api_view(["GET"])
+def update_forecasting(request):
+    try:
+        refine_xml_from_forecasting_search()
+    except Exception:  # TODO: 구체적인 Exception으로 변경할 것
+        raise ParseError(detail="fail to update forecasting")
+
+    return Response(status=status.HTTP_200_OK)
