609a35437b137a0f0e2bbc3d79db6742a97c9d50
MJbae <bmj5208@gmail.com>
2022-02-06 18:49:43 +0900

    feat: link functional view in path

--- forecasting/urls.py
+++ forecasting/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from . import views
+from .views import update_forecasting
 
 router = DefaultRouter()
 router.register("forecastings", views.ForecastingViewSet)
 
 urlpatterns = [
+    path("api/manual-update", update_forecasting, name="update_forecasting"),
     path("api/", include(router.urls)),
 ]
