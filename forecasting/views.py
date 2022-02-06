250099138127c9f1a2abaf41000ae65f64efa0e8
MJbae <bmj5208@gmail.com>
2022-02-06 16:05:20 +0900

    feat: add forecasting viewset

--- forecasting/views.py
+++ forecasting/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
 
-# Create your views here.
+from forecasting.models import Forecasting
+from forecasting.serializers import ForecastingSerializer
+
+
+class ForecastingViewSet(ModelViewSet):
+    queryset = Forecasting.objects.all()
+    serializer_class = ForecastingSerializer
