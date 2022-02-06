257d099006827353f636c7a4baf9911840cc001f
MJbae <bmj5208@gmail.com>
2022-02-06 15:17:46 +0900

    fix: remove useless models in accounts admin

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,24 +1,7 @@
 from django.contrib import admin
 from .models import User
-from .models.crops import Crop, ProducingCrop
-from .models.users import Farm
 
 
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
-    pass
-
-
-@admin.register(Farm)
-class FarmAdmin(admin.ModelAdmin):
-    pass
-
-
-@admin.register(Crop)
-class CropAdmin(admin.ModelAdmin):
-    pass
-
-
-@admin.register(ProducingCrop)
-class ProducingCropAdmin(admin.ModelAdmin):
     pass
