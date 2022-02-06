a17a5c1daac94e4e7c4aaf767258c664057e4486
MJbae <bmj5208@gmail.com>
2022-02-06 15:36:42 +0900

    feat: add Forecasting Model

---
+++ forecasting/models/forecasting.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+
+class Forecasting(models.Model):
+    sigungu_code = models.CharField(max_length=16, blank=True)
+    sigungu_name = models.CharField(max_length=16, blank=True)
+    crop_name = models.CharField(max_length=16, blank=True)
+    crop_code = models.CharField(max_length=16, blank=True)
+    target = models.CharField(max_length=16, blank=True)
+    susceptibility = models.FloatField()
+    generated_area = models.FloatField()
+    area_ratio = models.FloatField()
+    damage_rate = models.FloatField()
+
+    def __str__(self):
+        return f"'{self.crop_name} in {self.sigungu_name} damaged by {self.target}"
+
+    class Meta:
+        ordering = ["-id"]
+        db_table = 'forecasting'
