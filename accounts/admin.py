fb748b593d60b4217e91b3f3837a47ac9f21c354
MJbae <bmj5208@gmail.com>
2022-02-06 10:23:08 +0900

    feat: Add models in admin

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,7 +1,24 @@
 from django.contrib import admin
 from .models import User
+from .models.crops import Crop, ProducingCrop
+from .models.users import Farm
 
 
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Farm)
+class FarmAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Crop)
+class CropAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(ProducingCrop)
+class ProducingCropAdmin(admin.ModelAdmin):
     pass
