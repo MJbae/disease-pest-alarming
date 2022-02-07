9ca429a6861ad2b1feb0150f27fadff7bcf944bd
maxwell <maxwell@aidkr.com>
2022-02-07 15:09:25 +0900

    fix: change project permission to allow any for developing production

--- backend/settings/common.py
+++ backend/settings/common.py
@@ -114,7 +114,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 REST_FRAMEWORK = {
-    "DEFAULT_PERMISSION_CLASSES": ["rest_framework.permissions.IsAuthenticated", ],
+    "DEFAULT_PERMISSION_CLASSES": ["rest_framework.permissions.AllowAny", ],  # TODO: AllowAny -> IsAuthenticated 추후변경
     "DEFAULT_AUTHENTICATION_CLASSES": [
         "rest_framework_jwt.authentication.JSONWebTokenAuthentication",
         "rest_framework.authentication.SessionAuthentication",
