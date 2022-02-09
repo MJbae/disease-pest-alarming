31629c959f63905839e5a0d10f5bb7646438cd9b
maxwell <maxwell@aidkr.com>
2022-02-09 09:59:42 +0900

    fix: set proper path of setting

--- backend/celery.py
+++ backend/celery.py
@@ -4,7 +4,7 @@ from celery.schedules import crontab
 
 import os
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "backend.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "backend.settings.common")
 app = Celery("backend")
 
 app.config_from_object("django.conf:settings", namespace="CELERY")
