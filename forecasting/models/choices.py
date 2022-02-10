bbe13a9196309bab08499d23f8218b120b5b82e4
maxwell <maxwell@aidkr.com>
2022-02-10 10:32:37 +0900

    fix: change choices of address

--- forecasting/models/choices.py
+++ forecasting/models/choices.py
@@ -2,12 +2,12 @@ from django.db import models
 
 
 class LargeCategoryAddressChoices(models.TextChoices):
-    SEOUL = "Seoul", "서울특별시"
-    GYEONGGI = "Gyeonggi-do", "경기도",
+    SEOUL = "서울특별시", "서울특별시"
+    GYEONGGI = "경기도", "경기도"
 
 
 class MediumCategoryAddressChoices(models.TextChoices):
-    HWASEONG = "Hwaseong-si", "화성시"
-    SUWON = "Suwon-si", "수원시"
-    SEONGNAM = "Seongnam-si", "성남시"
-    ANYANG = "Anyang-si", "안양시"
+    HWASEONG = "화성시", "화성시"
+    SUWON = "수원시", "수원시"
+    SEONGNAM = "성남시", "성남시"
+    ANYANG = "안양시", "안양시"
