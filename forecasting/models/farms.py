5560fcb2dae267d687bb2b930826c5e072f930f2
maxwell <maxwell@aidkr.com>
2022-02-10 09:09:37 +0900

    fix: change class __str__ removing farm's name and migrate

--- forecasting/models/farms.py
+++ forecasting/models/farms.py
@@ -8,15 +8,12 @@ class Farm(models.Model):
     owner = models.ForeignKey(
         AUTH_USER_MODEL, related_name="farm_set", on_delete=models.CASCADE
     )
-    name = models.CharField(
-        max_length=13,
-        blank=True,
-    )
+
     large_category_address = models.CharField(max_length=15, blank=True, choices=LargeCategoryAddressChoices.choices)
     medium_category_address = models.CharField(max_length=15, blank=True, choices=MediumCategoryAddressChoices.choices)
 
     def __str__(self):
-        return f"{self.owner.__str__()}'s {self.name}"
+        return f"{self.owner.__str__()}농장"
 
     class Meta:
         ordering = ["-id"]
