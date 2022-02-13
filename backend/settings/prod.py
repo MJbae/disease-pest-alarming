dda60f1d484699f25e8b4eb44d5c81d9b6404d12
MJbae <bmj5208@gmail.com>
2022-02-13 21:42:35 +0900

    fix: add allowed_host env for resolving 400 bad request issue

--- backend/settings/prod.py
+++ backend/settings/prod.py
@@ -1,6 +1,7 @@
 from .common import *
 
 DEBUG = os.environ.get("DEBUG") in ["1", "t", "true", "T", "True"]
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "").split(",")
 
 STATICFILES_STORAGE = "backend.storages.StaticAzureStorage"
 DEFAULT_FILE_STORAGE = "backend.storages.MediaAzureStorage"
