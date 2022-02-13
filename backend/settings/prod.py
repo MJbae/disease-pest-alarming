2115a70bc57013347ea78bb427c27bee3b800cf2
MJbae <bmj5208@gmail.com>
2022-02-13 20:51:27 +0900

    feat: add azure storage env variables

--- backend/settings/prod.py
+++ backend/settings/prod.py
@@ -1,3 +1,9 @@
 from .common import *
 
 DEBUG = os.environ.get("DEBUG") in ["1", "t", "true", "T", "True"]
+
+STATICFILES_STORAGE = "backend.storages.StaticAzureStorage"
+DEFAULT_FILE_STORAGE = "backend.storages.MediaAzureStorage"
+
+AZURE_ACCOUNT_NAME = os.environ.get("AZURE_ACCOUNT_NAME")
+AZURE_ACCOUNT_KEY = os.environ.get("AZURE_ACCOUNT_KEY")
