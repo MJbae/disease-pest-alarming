0496cfaeb6eff91307f5c3b2ed9b89bf6f85d6c0
maxwell <maxwell@aidkr.com>
2022-02-14 11:04:52 +0900

    refactor: move logic of hashing password from serializer to view

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -7,18 +7,6 @@ User = get_user_model()
 class SignupSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True)
 
-    def create(self, validated_data):
-        user = User.objects.create(
-            username=validated_data["username"],
-            phone_number=validated_data["phone_number"]
-        )
-        user.set_password(validated_data["password"])
-        user.save()
-        owner_id = User.objects.get(username=user.username).id
-        print(f'owner_id: {owner_id}')
-
-        return user
-
     class Meta:
         model = User
         fields = ["pk", "username", "password", "phone_number"]
--- accounts/views.py
+++ accounts/views.py
@@ -1,4 +1,5 @@
 from django.contrib.auth import get_user_model
+from django.contrib.auth.hashers import make_password
 from rest_framework.permissions import AllowAny
 from rest_framework.generics import CreateAPIView
 from .serializers import SignupSerializer
@@ -10,3 +11,10 @@ class SignupView(CreateAPIView):
     permission_classes = [
         AllowAny,
     ]
+
+    def perform_create(self, serializer):
+        raw_password = serializer.validated_data.get("password")
+        hashed_password = make_password(raw_password)
+        print(f'password: {raw_password}')
+        print(f'username: {hashed_password}')
+        serializer.save(password=hashed_password)
