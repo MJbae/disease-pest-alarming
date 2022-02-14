e34597c58d8e078a81edb7fbd273b48886262cd8
maxwell <maxwell@aidkr.com>
2022-02-14 12:20:36 +0900

    feat: add logic of saving farms, crops with signup view

--- accounts/views.py
+++ accounts/views.py
@@ -2,6 +2,9 @@ from django.contrib.auth import get_user_model
 from django.contrib.auth.hashers import make_password
 from rest_framework.permissions import AllowAny
 from rest_framework.generics import CreateAPIView
+
+from forecasting.models import Crop
+from forecasting.serializers import FarmSerializer, ProducingCropSerializer
 from .serializers import SignupSerializer
 
 
@@ -15,6 +18,25 @@ class SignupView(CreateAPIView):
     def perform_create(self, serializer):
         raw_password = serializer.validated_data.get("password")
         hashed_password = make_password(raw_password)
-        print(f'password: {raw_password}')
-        print(f'username: {hashed_password}')
-        serializer.save(password=hashed_password)
+        username = serializer.save(password=hashed_password)
+        user = get_user_model().objects.get(username=username)
+        farms = self.request.data.get("farms")
+
+        for farm in farms:
+            farm["owner"] = user.id
+            crops = farm.get("producing_crops")
+
+            result = FarmSerializer(data=farm)
+            result.is_valid()
+            farm_instance = result.save()
+
+            for crop in crops:
+                crop_name = crop.get("name")
+                crop["farm"] = farm_instance.pk
+                crop_code = Crop.objects.get(name=crop_name).code
+                crop["crop"] = crop_code
+                del(crop['name'])
+
+                serialized_crop = ProducingCropSerializer(data=crop)
+                serialized_crop.is_valid()
+                serialized_crop.save()
