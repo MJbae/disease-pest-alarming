2c1763e5ab61f829e77c07f2ef662b07645043ae
maxwell <maxwell@aidkr.com>
2022-02-15 16:02:06 +0900

    feat: add index view url pattern

--- backend/urls.py
+++ backend/urls.py
@@ -16,8 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from accounts.views import index
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("forecasting/", include("forecasting.urls")),
     path("accounts/", include("accounts.urls")),
+    path('', index, name='index'),
 ]
