522901d283b7a64abc9f22a260b5b32059b4cbb4
maxwell <maxwell@aidkr.com>
2022-02-15 16:02:19 +0900

    chore: add template path in settings

--- backend/settings/common.py
+++ backend/settings/common.py
@@ -48,7 +48,7 @@ ROOT_URLCONF = 'backend.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
