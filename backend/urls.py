9d4798ee2ea4975491415b2fd9ec1f877a2ef05a
maxwell <maxwell@aidkr.com>
2022-02-18 09:14:50 +0900

    fix: add / in signup page url pattern

--- backend/urls.py
+++ backend/urls.py
@@ -38,7 +38,7 @@ urlpatterns = [
     path("api/v1/forecasting/", include("forecasting.urls")),
     path("api/v1/accounts/", include("accounts.urls")),
     path('admin/', admin.site.urls),
-    path('signup', signup, name='signup'),
+    path('signup/', signup, name='signup'),
     path('swagger/', schema_view.with_ui(
         'swagger', cache_timeout=0
     ), name='schema-swagger-ui'),
