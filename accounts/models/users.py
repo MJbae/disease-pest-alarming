918c9e04b9adceba73eb7b3047a24a6441a3b2a1
MJbae <bmj5208@gmail.com>
2022-02-20 16:41:48 +0900

    feat: update user model according to v1 erd

--- accounts/models/users.py
+++ accounts/models/users.py
@@ -1,10 +1,36 @@
 from django.contrib.auth.validators import UnicodeUsernameValidator
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, UserManager
 from django.core.validators import RegexValidator
 from django.db import models
+from django.utils import timezone
 
 
-class User(AbstractUser):
+class User(AbstractBaseUser, PermissionsMixin):
+    username_validator = UnicodeUsernameValidator()
+
+    username = models.CharField(
+        'username',
+        max_length=150,
+        unique=True,
+        help_text='Required. 150 characters or fewer. Letters, digits and @/./+/-/_ only.',
+        validators=[username_validator],
+        error_messages={
+            'unique': "A user with that username already exists.",
+        },
+    )
+    is_staff = models.BooleanField(
+        'staff status',
+        default=False,
+        help_text='Designates whether the user can log into this admin site.'
+    )
+    is_active = models.BooleanField(
+        'active',
+        default=True,
+        help_text=
+        'Designates whether this user should be treated as active. '
+        'Unselect this instead of deleting accounts.'
+    )
+    date_joined = models.DateTimeField('date joined', default=timezone.now)
     phone_number = models.CharField(
         max_length=13,
         unique=True,
@@ -12,8 +38,13 @@ class User(AbstractUser):
         validators=[RegexValidator(r"^010-?[1-9]\d{3}-?\d{4}$")],
     )
 
+    email = models.EmailField('email address', blank=True)
+    objects = UserManager()
+    USERNAME_FIELD = 'username'
+
     def __str__(self):
-        return self.username
+        return super().__str__()
 
     class Meta:
+        db_table = "user"
         ordering = ["-id"]
