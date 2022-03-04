32b80e1083a1a35051ddec5d549bd772513a252c
MJbae <bmj5208@gmail.com>
2022-03-04 22:23:15 +0900

    test: add test_find_affected_farms

--- v2/tests/test_services.py
+++ v2/tests/test_services.py
@@ -1,15 +1,16 @@
-from datetime import datetime
+from datetime import datetime, date
 
 import pytest
 from model_bakery import baker
 
-pytestmark = [pytest.mark.django_db]
+from ..forecasting.domains import ForecastingDto
+from ..forecasting.services import collect_the_latest_forecasting, find_affected_farms
 
-from forecasting.services import collect_the_latest_forecasting
+pytestmark = [pytest.mark.django_db]
 
 
 def test_collect_the_latest_forecasting():
-    # 1. forecasting mocking
+    # 1. mock forecasting
     latest_date = datetime.strptime("2021-11-02", "%Y-%m-%d").date()
     earliest_date = datetime.strptime("2021-10-26", "%Y-%m-%d").date()
 
@@ -24,12 +25,20 @@ def test_collect_the_latest_forecasting():
         assert forecasting.date > forecasting_from_db.date
 
 
-def test_extract_influential_forecasting():
-    pass
-
-
 def test_find_affected_farms():
-    pass
+    # mock forecasting dto set
+    forecasting_set = set()
+    forecasting_dto_1st = ForecastingDto(date=date.today(), target_name="test_pest", crop_name="포도", address_name="가평군")
+    forecasting_dto_2nd = ForecastingDto(date=date.today(), target_name="test_pest", crop_name="포도", address_name="화성시")
+    forecasting_set.add(forecasting_dto_1st)
+    forecasting_set.add(forecasting_dto_2nd)
+
+    # mock Farm and Owner
+
+    # return affected farms
+    farm_set = find_affected_farms(forecasting_set)
+
+    # test returned farms are valid
 
 
 def test_send_alarms():
