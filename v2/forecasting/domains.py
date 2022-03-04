7a2e8f81637d6c6ab8db613ab42505e2e07d3bc6
maxwell <maxwell@aidkr.com>
2022-03-04 14:29:11 +0900

    feat: add value objects of domain models

--- v2/forecasting/domains.py
+++ v2/forecasting/domains.py
@@ -11,3 +11,16 @@ class Address:
     code: str
     name: str
 
+
+@dataclass(unsafe_hash=True)
+class ForecastingDto:
+    target: str
+    date: date
+    crop: Crop
+    address: Address
+
+
+@dataclass(unsafe_hash=True)
+class AffectedFarmDto:
+    contact: str
+    info: ForecastingDto
