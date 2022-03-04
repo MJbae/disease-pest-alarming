b451efd8d658105c0482a2d3501b95f547cee265
maxwell <maxwell@aidkr.com>
2022-03-04 15:20:13 +0900

    test: add service logic test forms

---
+++ v2/tests/test_services.py
@@ -0,0 +1,14 @@
+def test_collect_the_latest_forecasting():
+    pass
+
+
+def test_extract_influential_forecasting():
+    pass
+
+
+def test_find_affected_farms():
+    pass
+
+
+def test_send_alarms():
+    pass
