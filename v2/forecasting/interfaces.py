49c0f2a9448cdaab27c9fbbd05aa9f0c9b0e11a3
maxwell <maxwell@aidkr.com>
2022-03-04 14:10:31 +0900

    fix: rename methods and vars

--- v2/forecasting/interfaces.py
+++ v2/forecasting/interfaces.py
@@ -1,28 +1,28 @@
 import abc
-from domains import ForecastingDto, DamageDto, DamagedFarmDto
+from domains import ForecastingDto, AffectedFarmDto
 from typing import Set
 
 
 class ICollector(abc.ABC):
     @abc.abstractmethod
-    def collect_the_latest_forecasting(self) -> ForecastingDto:
+    def collect_the_latest_forecasting(self) -> Set[ForecastingDto]:
         raise NotImplementedError
 
 
 class IDamageExtractor(abc.ABC):
     @abc.abstractmethod
-    def extract_damages(self, forecasting: ForecastingDto) -> Set[DamageDto]:
+    def extract_influential_forecasting(self, forecasting: ForecastingDto) -> Set[ForecastingDto]:
         raise NotImplementedError
 
 
 class IFarm(abc.ABC):
     @abc.abstractmethod
-    def find_damaged_farms(self, damages: Set[DamageDto]) -> Set[DamagedFarmDto]:
+    def find_affected_farms(self, damages: Set[ForecastingDto]) -> Set[AffectedFarmDto]:
         raise NotImplementedError
 
 
 class IAlarm(abc.ABC):
     @abc.abstractmethod
-    def send_alarm(self, farms: Set[DamagedFarmDto]):
+    def send_alarm(self, farms: Set[AffectedFarmDto]):
         raise NotImplementedError
 
