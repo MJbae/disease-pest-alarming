ea58e5aa92c0c0a7826c06cbe6b280913759b1b1
maxwell <maxwell@aidkr.com>
2022-03-04 16:25:32 +0900

    test: add steps for test_collect_the_latest_forecasting

--- v2/tests/test_services.py
+++ v2/tests/test_services.py
@@ -1,4 +1,10 @@
 def test_collect_the_latest_forecasting():
+    # 1. OPEN API 반환값  mocking
+
+    # 2. 최신 예찰정보를 다음의 형태로 반환, Set[ForecastingDto]
+    # forecasting_set = collect_the_latest_forecasting()
+
+    # 3. forecasting_set 내 모든 구성요소의 date가 최신 날짜를 가리키는지 체크
     pass
 
 
