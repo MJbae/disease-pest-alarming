d9c2519894285cad5c9314c9135b2a6202af03f0
MJbae <bmj5208@gmail.com>
2022-03-09 12:50:11 +0900

    feat: handle no elements set with logger

--- v2/forecasting/tasks.py
+++ v2/forecasting/tasks.py
@@ -5,12 +5,22 @@ from forecasting.services.forecasting import collect_the_latest_forecasting
 from forecasting.services.farm import find_affected_farms
 from forecasting.services.alarm import send_alarms
 
+from forecasting.exceptions import DateNotFoundException, NotLatestException, FarmNotAffectedException
+
 logger = logging.getLogger('celery')
 
 
 @shared_task
 def c_process_the_latest_forecasting():
     forecasting_set = collect_the_latest_forecasting()
+    if not forecasting_set:
+        logger.info(msg=f'latest forecasting does not exists')
+        return None
+
     farm_set = find_affected_farms(forecasting_set)
+    if not farm_set:
+        logger.info(msg=f'affected farm does not exists')
+        return None
+
     result, success_to_send = send_alarms(farm_set)
     logger.info(msg=f'result: {result}, success_to_send: {success_to_send}')
