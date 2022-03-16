c93d3a260e4a1b819d976aaf8718be260210f8b6
maxwell <maxwell@aidkr.com>
2022-03-16 10:34:56 +0900

    fix: change celery config according to its version(3.1.25)

--- v2/config/celery.py
+++ v2/config/celery.py
@@ -3,7 +3,7 @@ import os
 from celery import Celery
 from celery.schedules import crontab
 
-from config.settings.common import DJANGO_ENV
+from config.settings.common import DJANGO_ENV, INSTALLED_APPS
 
 if DJANGO_ENV == "dev":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.dev")
@@ -12,9 +12,9 @@ if DJANGO_ENV == "prod":
 
 app = Celery("config")
 
-app.config_from_object("django.conf:settings", namespace="CELERY")
+app.config_from_object("django.conf:settings")
 
-app.autodiscover_tasks()
+app.autodiscover_tasks(INSTALLED_APPS)
 
 app.conf.beat_schedule = {
     "process_the_latest_forecasting": {
